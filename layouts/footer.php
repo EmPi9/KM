c27072d57291318dc1a564d52f0e169a72d06673
empifoc123456789@gmail.com <empifoc123456789@gmail.com>
2024-03-15 14:31:05 +0300

    Сверстанный сайт без админки

---
+++ layouts/footer.php
@@ -0,0 +1,24 @@
+<footer class="text-gray-900 body-font p-6">
+            <div class="max-w-[1820px] mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+              <a class="flex flex-col w-96 title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <img src="../assets/img/logo 1.png" alt="">
+                <p class="pt-2">
+                  Улучшим вашу текущую автоматизированную производственную систему или разработаем совершенно новую.
+                </p>
+              </a>
+              <div class="md:ml-auto md:mr-auto flex flex-col flex-wrap gap-3 text-base justify-center underline-animation text-gray-900 hover:text-gray-700 relative transition duration-300 ease-in-out md:my-2">
+                <a class="mr-5 uppercase font-medium text-[24px] text-left cursor-pointer hover:text-[#392D88] hover:transition-all ease-in-out">ГЛАВНАЯ</a>
+                <a class="mr-5 uppercase font-medium text-[24px] text-left cursor-pointer hover:text-[#392D88] hover:transition-all ease-in-out">ПРОДУКТЫ</a>
+                <a class="mr-5 uppercase font-medium text-[24px] text-left cursor-pointer hover:text-[#392D88] hover:transition-all ease-in-out">ПРОЕКТЫ</a>
+                <a class="mr-5 uppercase font-medium text-[24px] text-left cursor-pointer hover:text-[#392D88] hover:transition-all ease-in-out">КОНТАКТЫ</a>
+                <a class="mr-5 uppercase font-medium text-[24px] text-left cursor-pointer hover:text-[#392D88] hover:transition-all ease-in-out">ВАКНСИИ</a>
+              </div>
+              <div class="md:ml-auto md:mr-auto flex flex-col flex-wrap gap-3 text-base justify-center underline-animation text-gray-900 hover:text-gray-700 relative transition duration-300 ease-in-out md:my-2"">
+                <p class="text-[36px] font-medium pb-5"><span>Телефон</span> : <span class="text-black font-semibold">8 (8553) 312-668</span></p>
+                <p class="text-[36px] font-medium pb-5"><span>Электронная почта:</span>  <span class="text-black font-semibold">info@tatkm.ru</span></p>
+                <a href="./send_request.html" class="bg-[#392D88] text-white text-center font-bold px-10 py-4 rounded-xl border-2 border-[#392D88] hover:bg-white hover:text-[#392D88] transition-all duration-700">Оставить заявку</a>
+              </div>
+            </div>
+          </footer> 
+</body>
+</html>
